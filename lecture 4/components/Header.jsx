27a2d1dd737c6bcb62a92255385d9f64aec3f6bf
deepanshu baghel <106313726+Deepanshuu06@users.noreply.github.com>
2024-03-22 00:02:43 +0530

    Update code formatting and fix minor bugs

---
+++ lecture 4/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <div className="nav-bar">
+        <h2>Food Adda</h2>
+        <ul className="nav-list">
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Header;
