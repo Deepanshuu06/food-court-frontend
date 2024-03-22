9a5bbce1383b9c6f7e5bd20a15ef6efd3987ae25
deepanshu baghel <106313726+Deepanshuu06@users.noreply.github.com>
2024-03-22 19:08:01 +0530

    Update file paths and remove unused files

--- lets get hooked/src/appLayout.js
+++ lets get hooked/src/appLayout.js
@@ -2,16 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Header from "./components/Header";
-import Body from "./components/Body";
-import NotFound from "./components/NotFound"; // Import NotFound component if used
+import Header from "../components/Header";
+import Body from "../components/Body";
+import NotFound from "../components/NotFound"; // Import NotFound component if used
 
 const Layout = () => {
   return (
     <>
       <Header />
       <Body />
-
     </>
   );
 };
