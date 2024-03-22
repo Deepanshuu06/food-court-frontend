2ade921fe8bf0b74d8bac9bbebfd511bb4c4e56a
deepanshu baghel <106313726+Deepanshuu06@users.noreply.github.com>
2024-03-22 18:20:25 +0530

    Fix import paths and add new dependencies

--- lecture 4/appLayout.js
+++ lecture 4/appLayout.js
@@ -1,17 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header";
-import Body from "./components/Body"
+import Body from "./components/Body";
+import NotFound from "./components/NotFound"; // Import NotFound component if used
 
-const layout = () => {
-    return (
-        <>
-            <Header />
-            <Body />
-        </>
-    );
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+
+    </>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(layout());
+root.render(<Layout />);
--- lecture 4/components/Body.jsx
+++ lecture 4/components/Body.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import RestorantCard from " ./RestorantCard";
+import RestorantCard from "./RestorantCard";
 
 function Body() {
   const restaurantlist = [
