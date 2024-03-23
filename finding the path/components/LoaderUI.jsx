51edb12ff1c469d4bf834b5df459e19e3ab248f5
deepanshu baghel <106313726+Deepanshuu06@users.noreply.github.com>
2024-03-23 23:47:38 +0530

    Update .DS_Store file

---
+++ finding the path/components/LoaderUI.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const NotFound = () => {
+    return(
+     <div className="shimmer-wrapper">
+      <h1>Shimmer ui is loading</h1>
+     </div>
+    )
+}
+
+export default NotFound;
