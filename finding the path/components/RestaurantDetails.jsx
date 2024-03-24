7207e128789a1637b0a35a609eb069918430ab47
deepanshu baghel <106313726+Deepanshuu06@users.noreply.github.com>
2024-03-24 17:45:36 +0530

    Add restaurant details and fetch data from API

--- finding the path/components/RestaurantDetails.jsx
+++ finding the path/components/RestaurantDetails.jsx
@@ -1,8 +1,36 @@
+import { useParams } from "react-router";
+import React from "react";
+import { useEffect, useState } from "react";
+
 const RestaurantMenu = () => {
+//   const { id } = useParams();
+
+  const [restaurantdata, setRestaurantData] = useState({});
+
+  useEffect(() => {
+    fetchRestaurantData();
+  }, []);
+
+  async function fetchRestaurantData() {
+    const response = await fetch(
+      `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.71700&lng=75.83370&restaurantId=84070&catalog_qa=undefined&submitAction=ENTER`
+    );
+    const json = await response.json();
+    setRestaurantData(json);
+  }
+
+  const { name , city } = restaurantdata?.data?.cards[0]?.card?.card?.info
+
+// console.log(restaurantdata.data.cards[0].card.card.info.name);
+console.log(restaurantdata?.data?.cards[0]?.card?.card?.info?.name);
+
   return (
     <>
-      <h2>restaurant id : 12131</h2>
-      <h2>Restaurant Menu</h2>
+        <div className="restaurant-menu">
+            <h1>{name}</h1>
+            <h1>{city}</h1>
+            
+        </div>
     </>
   );
 };
