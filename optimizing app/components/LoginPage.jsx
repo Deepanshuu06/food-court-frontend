e842eb5c7f47c10eac28aa6ece521fc26a97052d
deepanshu baghel <106313726+Deepanshuu06@users.noreply.github.com>
2024-04-03 09:42:41 +0530

    adding questions

--- optimizing app/components/LoginPage.jsx
+++ optimizing app/components/LoginPage.jsx
@@ -1,8 +1,47 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
 const LoginPage = ()=>{
     return(
-        <>
-        <h1>Login Page</h1>
-        </>
-    )
+        <div>
+          <h1>Login</h1>
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            validationSchema={Yup.object({
+              email: Yup.string()
+                .email('Invalid email address')
+                .required('Email is required'),
+              password: Yup.string()
+                .min(6, 'Password must be at least 6 characters')
+                .required('Password is required'),
+            })}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({ isSubmitting }) => (
+              <Form>
+                <div>
+                  <label htmlFor="email">Email</label>
+                  <Field type="email" name="email" />
+                  <ErrorMessage name="email" component="div" />
+                </div>
+                <div>
+                  <label htmlFor="password">Password</label>
+                  <Field type="password" name="password" />
+                  <ErrorMessage name="password" component="div" />
+                </div>
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      );
 }
 export default LoginPage;
